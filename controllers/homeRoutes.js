4ed3a1a2fc5b6aa0994e34afae9f03e5374133be
EleazarF <eleazar.fuentes@gmail.com>
2023-09-19 10:16:44 -0600

    Update homeRoutes.js
    adding comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,23 @@
+// Import necessary dependencies and models
 const router = require("express").Router();
 const { Post, User } = require("../models");
 const withAuth = require("../utils/auth");
 
+// Define a route to render the homepage
 router.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("homepage"); // Render the "homepage" view
 });
 
+// Define a route to render the login page
 router.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login"); // Render the "login" view
 });
 
+// Define a route to render the signup page
 router.get("/signup", (req, res) => {
-  res.render("signup");
+  res.render("signup"); // Render the "signup" view
 });
+
+// Export the router for use in other parts of the application
 module.exports = router;
+
